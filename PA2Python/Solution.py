5a8d2851e2ff2620ae54d029016fe00a654f1de3
aarav <aarav.kamat5@gmail.com>
2026-03-10 01:37:25 -0400

    pa2start

---
+++ PA2Python/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def __init__(self, start_node, graph):
+        self.start_node = start_node
+        self.graph = graph
+
+    def output_distances(self):
+        """
+        :return: the list of minimum distances from each node to the start node
+        """
+        return [] #Return empty
